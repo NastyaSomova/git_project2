3f85e1361b46477fa688ca92a58075bf47bf0d36
Nastya <anastrasd@yandex.ru>
2020-03-02 20:50:45 +0300

    Commit in new branch2

--- program.py
+++ program.py
@@ -3,7 +3,7 @@ def main():
     print('And I change it every day')
     print('And I change it every day 2')
     print('And I change it every day 2')
-    print('New Branch')
+    print('New Branch2')
     
 if __name__ == '__main__':
     main()
